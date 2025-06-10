449d94b4e0770ef6940a5f3ddf00b2c359f0afc5
Aaqil <aaqil.ruzzan@gmail.com>
2025-06-11 00:22:25 +0530

    Add Call to Action, Discover, and Footer sections to the landing page, integrating FontAwesome icons for enhanced visual appeal and user interaction.

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
+import FooterSection from "./FooterSection";
 const LandingPage = () => {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
+      <FooterSection />
     </div>
   );
 };
